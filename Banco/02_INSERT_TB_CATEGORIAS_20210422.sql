263a9f2aec9999ddc12524fc01d19eb034433e8d
Guilherme Dorazzi <guilhermedorazzi@gmail.com>
2021-04-22 19:38:27 -0300

    Criação dos scripts das tabelas e de inserte de dados.

---
+++ Banco/02_INSERT_TB_CATEGORIAS_20210422.sql
@@ -0,0 +1,9 @@
+--
+INSERT INTO TB_CATEGORIAS(COD_CATEGORIA,NOM_CATEGORIA,FLG_ATIVO)
+VALUES (1,'Látex PVA','S')
+	  ,(2,'Tinta acrílica','S')
+      ,(3,'Tinta esmalte','S')
+      ,(4,'Tintas epóxi','N')
+      ,(5,'Tinta verniz','S');
+--
+COMMIT;
---
+++ Banco/04_INSERT_TB_PRODUTOS_20210422.sql
@@ -0,0 +1,14 @@
+--
+INSERT INTO TB_PRODUTOS(COD_PRODUTO,COD_CATEGORIA,COD_BARRAS,NOM_PRODUTO,DES_PRODUTO,FLG_ATIVO, VLR_PRODUTO)
+VALUES (1,1,'10001','Suvinil 3,6 litros','Tinta interna','S',10.99)
+	  ,(2,1,'10002','Renner 3,6 litros','Tinta externa','S',11.99)
+	  ,(1,2,'10003','Coral 10 litros','Tinta interna','S',12.99)
+	  ,(2,2,'10004','Sherwin Williams 1 litro','Tinta externa','S',13.99)
+	  ,(1,3,'10005','Eucatex 5 litros','Tinta interna','S',14.99)
+	  ,(2,3,'10006','Eucatex 1 litro','Tinta externa','S',15.99)
+	  ,(1,4,'10007','Suvinil 5 litros','Tinta interna','S',16.99)
+	  ,(2,4,'10008','Coral 5 litros','Tinta externa','S',17.99)
+	  ,(1,5,'10009','Renner 1 litro','Tinta interna','S',17.99)
+	  ,(2,5,'10010','Sherwin Williams 5 litros','Tinta externa','S',17.99);
+--
+COMMIT;
