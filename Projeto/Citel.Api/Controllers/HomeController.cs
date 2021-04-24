780010f1e28384326b491c92585445fc7c21f023
Guilherme Dorazzi <guilhermedorazzi@gmail.com>
2021-04-24 11:25:45 -0300

    Criação do projeto base com utilizando o DDD (Domain Driven Design)

---
+++ Projeto/Citel.Api/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Citel.Api.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("v1/Home")]
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
